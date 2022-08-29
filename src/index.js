566ede6981682d3d8f91b7c168d5fab576d91560
clay2 <clay@DESKTOP-B145MCP.localdomain>
2022-08-29 18:03:46 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -40,25 +40,46 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
-const navBar = document.getElementsByClassName('container')
-const cta = document.getElementsByClassName('cta')
-const main = document.getElementsByClassName('main-content')
-const contact = document.getElementsByClassName('contact')
-// contact.appendChild(document.createElement('h4'))
-
-// const h4 = contact.appendChild(document.createElement('h4'))
-// h4.textContent = 'Contact'
-
-
-
+// console.log('project wired!')
+// console.log(siteContent.nav["nav-item-1"])
 const aTags = document.querySelectorAll('a')
-aTags[0].textContent = "Services"
-aTags[1].textContent = "Product"
-aTags[2].textContent = "Vision"
-aTags[3].textContent = "Features"
-aTags[4].textContent = "About"
-aTags[5].textContent = "Contact"
+aTags[0].textContent = siteContent.nav["nav-item-1"]
+aTags[1].textContent = siteContent.nav["nav-item-2"]
+aTags[2].textContent = siteContent.nav["nav-item-3"]
+aTags[3].textContent = siteContent.nav["nav-item-4"]
+aTags[4].textContent = siteContent.nav["nav-item-5"]
+aTags[5].textContent = siteContent.nav["nav-item-6"]
+
+//CTA
+document.querySelector('h1').textContent = siteContent.cta['h1'];
+document.querySelector('button').textContent = siteContent.cta['button'];
+
+const headers = document.querySelectorAll('h4')
+const pTags = document.querySelectorAll('p')
+
+//FEATURES
+headers[0].textContent = siteContent["main-content"]["features-h4"]
+  pTags[0].textContent = siteContent["main-content"]["features-content"]
+//ABOUT
+headers[1].textContent = siteContent["main-content"]["about-h4"]
+  pTags[1].textContent = siteContent["main-content"]["about-content"]
+//SERVICES
+headers[2].textContent = siteContent["main-content"]["services-h4"]
+  pTags[2].textContent = siteContent["main-content"]["services-content"]
+//PRODUCT
+headers[3].textContent = siteContent["main-content"]["product-h4"]
+  pTags[3].textContent = siteContent["main-content"]["product-content"]
+//VISION
+headers[4].textContent = siteContent["main-content"]["vision-h4"]
+  pTags[4].textContent = siteContent["main-content"]["vision-content"]
+
+//CONTACT
+headers[5].textContent = siteContent.contact["contact-h4"]
+  pTags[5].textContent = siteContent.contact.address
+  pTags[6].textContent = siteContent.contact.phone
+  pTags[7].textContent = siteContent.contact.email
+
+
 
 
 
@@ -67,7 +88,7 @@ aTags.forEach(el => {
 })
 
 aTags[6].className = 'bold'
-aTags[6].textContent = "Copyright Great Idea! 2021"
+aTags[6].textContent = siteContent.footer.copyright
 
 
 const logo = document.getElementById('logo-img');
